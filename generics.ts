bfbe03ca6736a70c994221e4c3bf77ca9649f47a
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-03-15 13:55:55 +0100

    Add examples of type inference with generic function

--- generics.ts
+++ generics.ts
@@ -24,3 +24,26 @@ class ArrayOfAnything<T> {
 
 const arr = new ArrayOfAnything<string>(['a', 'b', 'c']);
 const arr2 = new ArrayOfAnything(['a', 'b', 'c']);
+
+// Examples of generics with functions
+
+function printStrings(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printAnything<string>(['a', 'b', 'c']);
+printAnything(['a', 'b', 'c']);
