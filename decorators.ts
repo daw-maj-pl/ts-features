68baef4d8f84e037c2a474e49d20d0f63114460c
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-04-12 10:42:20 +0200

    Use property descriptor

--- decorators.ts
+++ decorators.ts
@@ -1,21 +1,27 @@
 class Boat {
-  @testDecorator
   color: string = 'blue';
 
-  @testDecorator
   get formattedColor(): string {
     return `This boats color is ${this.color}`;
   }
 
-  @testDecorator
+  @logError
   pilot(): void {
+    throw new Error();
     console.log('swish');
   }
 }
 
-function testDecorator(target: any, key: string): void {
-  console.log('Target:', target);
-  console.log('Key:', key);
+function logError(target: any, key: string, desc: PropertyDescriptor): void {
+  const method = desc.value;
+
+  desc.value = function () {
+    try {
+      method();
+    } catch (e) {
+      console.log('Oops, boat was sunk');
+    }
+  };
 }
 
-// testDecorator(Boat.prototype, 'pilot');
+new Boat().pilot();
