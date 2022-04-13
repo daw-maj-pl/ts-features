eaecdb7f8a8224aae7c1a37ea52bb6c1c9a80806
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-04-13 20:08:21 +0200

    Put matadata property on fly method using decorator

--- metadata.ts
+++ metadata.ts
@@ -1,8 +1,8 @@
 import 'reflect-metadata';
 
-const plane = {
-  color: 'red'
-};
+// const plane = {
+//   color: 'red'
+// };
 
 // Reflect.defineMetadata('note', 'test', plane);
 // Reflect.defineMetadata('height', 70, plane);
@@ -13,8 +13,27 @@ const plane = {
 // console.log(note);
 // console.log(height);
 
-Reflect.defineMetadata('note', 'test2', plane, 'color');
+// Reflect.defineMetadata('note', 'test2', plane, 'color');
 
-const note = Reflect.getMetadata('note', plane, 'color');
+// const note = Reflect.getMetadata('note', plane, 'color');
 
-console.log(note);
+// console.log(note);
+
+class Plane {
+  color: string = 'red';
+
+  @markFunction('Test3')
+  fly(): void {
+    console.log('vrrr');
+  }
+}
+
+function markFunction(secretInfo: string) {
+  return function (target: Plane, key: string) {
+    Reflect.defineMetadata('secret', secretInfo, target, key);
+  };
+}
+
+const secret = Reflect.getMetadata('secret', Plane.prototype, 'fly');
+
+console.log(secret);
